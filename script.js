180ec4e2e5ef0f00669eeb7bf176c2bea7c03fd1
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-03-25 15:55:20 +0545

    chore: clean up code structures and add weather icons

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
+const cityInput = document.querySelector("#cityInput");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const btn = document.querySelector("#searchBtn");
+const img = document.querySelector('#icon');
 
 async function getWeather(city){
 	try{
 		const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8ff011f49cd00fec9b7dc0fda940c2f6&units=metric");
 		const data = await response.json();
-		if(data.cod === "404"){
-    document.querySelector('#description').textContent = "City not found";
-    return;
-}
+		if(data.cod === "404")
+		{
+	    	document.querySelector('#description').textContent = "City not found";
+	    	return;
+		}
+		img.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
 		document.querySelector('#city-name').textContent = city;
 		document.querySelector('#temp').textContent = data.main.temp + "C";
 		document.querySelector('#description').textContent = data.weather[0].description;
-		humidity.textContent = "Humidity : "+data.main.humidity + "%";
+		humidity.textContent = "Humidity : " + data.main.humidity + "%";
 		wind.textContent = "Wind-speed : " + data.wind.speed;
 	}
 	catch(error){
 		console.log(error)
 	}
 }
-const cityInput = document.querySelector("#cityInput");
-const humidity = document.querySelector("#humidity");
-const wind = document.querySelector("#wind");
-const btn = document.querySelector("#searchBtn");
-btn.addEventListener("click", function(){
-	city = cityInput.value;
-	getWeather(city);
-});
+
 cityInput.addEventListener("keydown",function(e){
 	if(e.key == "Enter"){
 		city = cityInput.value;
 		getWeather(city);
 	}
 })
+
+btn.addEventListener("click", function(){
+	city = cityInput.value;
+	getWeather(city);
+});
