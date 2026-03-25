2ef681d530a56e1e10feb817b6f1ee5bacbfa75a
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-03-25 15:16:41 +0545

    style: style weather app with glassmorphism design

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ async function getWeather(city){
     document.querySelector('#description').textContent = "City not found";
     return;
 }
-
+		document.querySelector('#city-name').textContent = city;
 		document.querySelector('#temp').textContent = data.main.temp + "C";
 		document.querySelector('#description').textContent = data.weather[0].description;
-
+		humidity.textContent = "Humidity : "+data.main.humidity + "%";
+		wind.textContent = "Wind-speed : " + data.wind.speed;
 	}
 	catch(error){
 		console.log(error)
 	}
 }
 const cityInput = document.querySelector("#cityInput");
-
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
 const btn = document.querySelector("#searchBtn");
 btn.addEventListener("click", function(){
 	city = cityInput.value;
 	getWeather(city);
 });
+cityInput.addEventListener("keydown",function(e){
+	if(e.key == "Enter"){
+		city = cityInput.value;
+		getWeather(city);
+	}
+})
